176aa9f267b38aa346daf8cdd532db5158622fd3
BenBrayan <bpbrayan69@gmail.com>
2024-01-25 13:49:00 -0500

    archivos back4

---
+++ Test-Employees-Api/Services/EmployeeService.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+using Test_Employees_Api.Models;
+
+namespace Test_Employees_Api.Services
+{
+    public interface IEmployeeService
+    {
+        public Task<Employee> GetEmployeeAnnualSalaryById(int id);
+        public Task<IEnumerable<Employee>> GetAllEmployeesAnnualSalary();
+    }
+
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IExternalApiService _externalApi;
+        public EmployeeService(IExternalApiService externalApi)
+        {
+            _externalApi = externalApi;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployeesAnnualSalary()
+        {
+            //Se consume la api externa y nos retorna las lista de empleados
+            List<Employee> employees = await _externalApi.GetAllEmployeesExternalApi();
+
+            //Se recorre la lista de empleados de la api externa donde se multiplica su salario por 12 meses para saber cuanto gana al año
+            employees.ForEach(x => x.employee_annual_salary = x.employee_salary * 12);   
+
+            return employees;
+        }
+        
+        public async Task<Employee> GetEmployeeAnnualSalaryById(int id)
+        {
+            //Se consume la api externa y nos retorna su empleado filtrado
+            Employee employee = await _externalApi.GetEmployeeByIdExternalApi(id);
+
+            //Se modifica la propiedad del empleado de la api externa donde se multiplica su salario por 12 meses para saber cuanto gana al año
+            employee.employee_annual_salary = employee.employee_salary * 12;
+
+            return employee;
+        }
+        
+    }
+}
---
+++ Test-Employees-Api/Services/ExternalApiService.cs
@@ -0,0 +1,68 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using Test_Employees_Api.Models;
+
+namespace Test_Employees_Api.Services
+{
+    public interface IExternalApiService
+    {
+        public Task<List<Employee>> GetAllEmployeesExternalApi();
+
+        public Task<Employee> GetEmployeeByIdExternalApi(int id);
+    }
+
+    public class ExternalApiService: IExternalApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ExternalApiService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("ExternalApi");
+        }
+        public async Task<List<Employee>> GetAllEmployeesExternalApi()
+        {
+            try
+            {
+                //URL de empleados
+                var response = await _httpClient.GetAsync($"v1/employees");
+
+                //Léctura del contenido
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+
+                //Se transforma el formato Json y se tipa la respuesta donde se retorna la data de los empleados
+                var apiResponse = JsonSerializer.Deserialize<ApiResponseAll>(content);
+
+                return apiResponse.data;
+            }
+            catch (HttpRequestException e)
+            {
+                // Manejar excepciones relacionadas con la solicitud HTTP
+                throw new InvalidOperationException("Error al consumir la api externa", e);
+            }
+        }
+
+        public async Task<Employee> GetEmployeeByIdExternalApi(int id)
+        {
+            try
+            {
+                //URL de empleado filtrado por Id
+                var response = await _httpClient.GetAsync($"v1/employee/{id}");
+
+                //Léctura del contenido
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+
+                //Se transforma el formato Json y se tipa la respuesta donde se retorna la data del empleado
+                var apiResponse = JsonSerializer.Deserialize<ApiResponseId>(content);
+
+                return apiResponse.data;
+            }
+            catch (HttpRequestException e)
+            {
+                // Manejar excepciones relacionadas con la solicitud HTTP
+                throw new InvalidOperationException("Error al consumir la api externa", e);
+            }
+        }
+    }
+}
