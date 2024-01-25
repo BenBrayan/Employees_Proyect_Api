12e7943882a50e9c1b5bc6a4265020ef51bfb36b
BenBrayan <bpbrayan69@gmail.com>
2024-01-25 13:47:47 -0500

    archivos back2

---
+++ Test-Employees-Api/Models/ApiResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Test_Employees_Api.Models
+{
+    public class ApiResponse
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+
+    }
+
+    public class ApiResponseId : ApiResponse
+    {
+        public Employee data { get; set; }
+    }
+
+    public class ApiResponseAll : ApiResponse
+    {
+        public List<Employee> data { get; set; }
+    }
+}
---
+++ Test-Employees-Api/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test_Employees_Api.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+        public string employee_name { get; set; }
+        public int employee_salary { get; set; }
+        public int employee_annual_salary { get; set; }
+        public int employee_age { get; set; }
+        public string profile_image { get; set; }
+    }
+}
